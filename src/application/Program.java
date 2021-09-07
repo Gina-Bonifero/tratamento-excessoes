a737727bec7f315118787456d081e533e6a707ac
Gina-Bonifero <gvbonifero@gmail.com>
2021-09-07 19:22:02 -0300

    exercicio de fixaçao de tratamento de excessoes

---
+++ src/application/Program.java
@@ -0,0 +1,38 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.AccountExceptions;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter account data");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		sc.nextLine();
+		System.out.print("Holder: ");
+		String holder = sc.nextLine();
+		System.out.print("Initial balance: ");
+		double balance = sc.nextDouble();
+		System.out.print("Withdraw limit: ");
+		double withdrawLimit = sc.nextDouble();
+		Account acc = new Account(number, holder, balance, withdrawLimit);
+		System.out.println();
+		System.out.print("Enter amount for withdraw: ");
+		double amount = sc.nextDouble();
+		
+		try {
+			acc.withdraw(amount);
+		} catch (AccountExceptions e) {
+			System.out.println("Withdraw error: " + e.getMessage());
+		}
+		
+		sc.close();
+	}
+
+}
